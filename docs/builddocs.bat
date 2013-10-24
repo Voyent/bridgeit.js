0c4f6593cc72b1dd4860ac755306925c1d0c679b
Philip Breau <philip.breau@icesoft.com>
2013-10-24 16:53:00 -0300

    built docs

--- docs/builddocs.bat
+++ docs/builddocs.bat
@@ -1,3 +1,3 @@
 cd /d %~dp0
-START /W jsduck --config jsduck-conf.json -o out
-cp images/ice-logo.png out/resources/images/ice-logo.png
+START /W jsduck --config jsduck-conf.json -o html
+cp images/ice-logo.png html/resources/images/ice-logo.png
