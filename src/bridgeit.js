95ccec8e2a7e4b2c2256d69924db17a99e494d3e
Philip Breau <philip.breau@icesoft.com>
2013-11-13 11:56:00 -0400

    add release version

--- src/bridgeit.js
+++ src/bridgeit.js
@@ -1,4 +1,5 @@
-/*
+/* BridgeIt Mobile 1.0.0 
+ *
  * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the
