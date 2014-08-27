914e733be65c921a4c7f933119bb1699c8108488
Ted Goddard <ted.goddard@icesoft.com>
2014-08-27 15:43:58 -0600

    enable windows phone push #42

--- src/bridgeit.js
+++ src/bridgeit.js
@@ -1486,7 +1486,7 @@ if (!window.console) {
             '6':   [true, true, true, true, true, true, false, true, false, false, false],
             '7':   [true, true, true, true, true, true, true,  true, false, true, true]
         },
-        'wp8':     [true, true, true, true, false, false, true, false, true, false, false],
+        'wp8':     [true, true, true, true, false, true, true, false, true, false, false],
         'android': [true, true,  true,  true, false, true,  true, true,  true, false, true]
     }
 
