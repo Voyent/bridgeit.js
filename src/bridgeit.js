3909af3f0f4c3c96d33267ca3b35532e8f36c697
Philip Breau <philip.breau@icesoft.com>
2014-10-01 15:01:32 -0300

    enable windows phone push #42
    re-add 914e733be65c921a4c7f933119bb1699c8108488

--- src/bridgeit.js
+++ src/bridgeit.js
@@ -1451,7 +1451,7 @@ if (!window.console) {
 			'7':   [true,     true,       true,        true,           true,  true,  true,  true,    false,  true,      true],
 			'8':   [true,     true,       true,        true,           true,  true,  true,  true,    false,  true,      true]
 		},
-		'wp8':     [true,     true,       true,        true,           false, false, true,  false,   true,   false,     false],
+		'wp8':     [true,     true,       true,        true,           false, true,  true,  false,   true,   false,     false],
 		'android': [true,     true,       true,        true,           false, true,  true,  true,    true,   false,     true]
 	}
 
