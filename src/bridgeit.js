7c49b63bcb4ab53e6dac1f8a3f18558a45b4653e
pcorless@gmail.com <pcorless@gmail.com>
2014-10-20 16:05:06 -0600

    ICE-10324 mobi:fetchContact doesn't work on first attempt with bridgeIt loaded via Ajax

--- src/bridgeit.js
+++ src/bridgeit.js
@@ -1,4 +1,4 @@
-/* BridgeIt Mobile 1.0.4
+/* BridgeIt Mobile 1.0.5
  *
  * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -1738,5 +1738,6 @@ if (!window.console) {
 	//android functions as full page load
 	addOnLoadListener(loadComplete);
 	addOnLoadListener(checkExecDeviceResponse);
+    loadComplete();
 	checkExecDeviceResponse();
 })(bridgeit);
