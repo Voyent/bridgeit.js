98413287181c7f07c1e31f4adfdd0c9575fa75a1
mircea.toma <mircea.toma@icesoft.com>
2018-01-11 01:44:50 +0200

    VRAS-128 Use the port as well when defaulting.

--- src/voyent.js
+++ src/voyent.js
@@ -32,7 +32,7 @@ if (!window.voyent) {
             if (privateUtils.isNode) {
                 v.baseURL = 'http://dev.voyent.cloud';
             } else {
-                v.baseURL = window.location.protocol + '//' + window.location.hostname;
+                v.baseURL = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port;
             }
         } else {
             v.baseURL = url;
